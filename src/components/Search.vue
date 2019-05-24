9c6e13cfa5a27390a020473500c22bdbf7275fe4
Dardan Meha <mdardan20@gmail.com>
2019-05-24 17:29:51 -0400

    added red/green coloring to complete/incomplete

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,63 +1,73 @@
 <template>
   <div class="search">
     <h5>Check Status:</h5>
-    <form v-on:submit.prevent="getResult(query)">
-      <input type="text" placeholder="Enter application number..." v-model="query" />
+    <form class="search-form" v-on:submit.prevent="getResult(query)">
+      <input type="text" placeholder="Enter application number..." v-model="query">
     </form>
 
     <div v-if="results">
-
       <div v-for="result in results" v-bind:key="result">
+        <strong>Current Status: </strong>
+        <span class="complete-green" v-if="result.status === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Application No:</strong> {{ result.app_no }}
+        <strong>Drug Test: </strong>
+        <span class="complete-green" v-if="result.drug_test === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Current Status:</strong> {{ result.status }}
+        <strong>Wav Course: </strong>
+                <span class="complete-green" v-if="result.wav_course === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Drug Test:</strong> {{ result.drug_test }}
+        <strong>Defensive Driving: </strong>
+        <span class="complete-green" v-if="result.defensive_driving === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Wav Course:</strong> {{ result.wav_course }}
+        <strong>Driver Exam: </strong>
+                <span class="complete-green" v-if="result.driver_exam === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Defensive Driving:</strong> {{ result.defensive_driving }}
+        <strong>Medical Clearance: </strong>
+                        <span class="complete-green" v-if="result.medical_clearance_form === 'Complete'">Complete</span>
+        <span class="incomplete-red" v-else>Incomplete</span>
         <br>
-        <strong>Driver Exam:</strong> {{ result.driver_exam }}
-        <br>
-        <strong>Medical Clearance:</strong> {{ result.medical_clearance_form }}
-        <br>
-        <strong>Other Requirements:</strong> 
+        <strong>Other Requirements: </strong>
         <br>
         {{ result.other_requirements }}
       </div>
-
     </div>
 
-         <br>
-      Source: 
-      <br>
-      <a href='https://data.cityofnewyork.us/resource/dpec-ucu7.json' target="_blank">NYC Open Data</a>
+    <br>Source:
+    <br>
+    <a href="https://data.cityofnewyork.us/resource/dpec-ucu7.json" target="_blank">NYC Open Data</a>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'search',
+  name: "search",
   data() {
     return {
-      msg: 'Search',
-      query: '',
-      results: ''
-    }
-  }, 
+      msg: "Search",
+      query: "",
+      results: ""
+    };
+  },
   methods: {
     getResult(query) {
-      axios.get('https://data.cityofnewyork.us/resource/dpec-ucu7.json?app_no='+query)
-      .then( respone => {
-        this.results = respone.data;
-      })
+      axios
+        .get(
+          "https://data.cityofnewyork.us/resource/dpec-ucu7.json?app_no=" +
+            query
+        )
+        .then(respone => {
+          this.results = respone.data;
+        });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -77,6 +87,21 @@ a {
   color: #42b983;
 }
 input {
-  max-width: 50%;
+  max-width: 30%;
+  text-align: center;
+}
+.search-form {
+  margin: 0 0 2%;
+}
+.complete-green {
+  font-weight: 700;
+  color: green;
+}
+.incomplete-red {
+  font-weight: 700;
+  color: red;
+}
+.search {
+  margin-top: 5%;
 }
 </style>
